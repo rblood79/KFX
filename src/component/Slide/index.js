b89e3ca485036bc85957d2a3b93f7da2b82523bc
rblood79 <rblood79@gmail.com>
2021-09-15 00:14:55 +0900

    filter

--- src/component/Slide/index.js
+++ src/component/Slide/index.js
@@ -11,7 +11,7 @@ import ExpendItem from './expendItem';
 import { useWindowSize, useGridNum, usePosition, useMove, shuffle } from '../Mixin';
 import _ from 'lodash';
 
-import { DS, } from '../Data';
+import { DS, temp } from '../Data';
 
 const App = (props) => {
   const state = useContext(context);
@@ -36,16 +36,56 @@ const App = (props) => {
     };
   }
 
+  function customizer(objValue) {
+    if (objValue === 'N') {
+      return 0;
+    }
+  }
+  function aver(arr, keys) {
+    let valueSum = 0;
+    _.map(arr, (v, k) => {
+      const found = keys.find(element => element === k);
+      if (found) {
+        valueSum += v
+      }
+    })
+    return valueSum;
+  }
   useEffect(() => {
     if (topNum === 0) {
-      const filterData = _.filter(DS, { LOCATION: '1B' });
+      //const filterData = _.filter(DS, { LOCATION: '1B' });
+      //setData(filterData)
+
       /*console.log(filterData)
       const skip = ['id', 'title', 'img', 'LOCATION', 'SERIES', 'MISSION']
       const xx = _.omit(DS, skip)
       var yy = _.map(filterData, function (n) {
         return _.omit(n, skip);
       });*/
-      setData(filterData)
+      //
+      const oData = temp;
+      const info = oData[0].부대;
+      const weight = _.cloneDeep(oData[0].가중치);
+      const used = _.cloneDeep(oData[0].기준정보);
+      const mUsed = _.mergeWith(used, weight, customizer);
+      const keyArray = Object.keys(used);
+      const sUsed = aver(mUsed, keyArray);
+      const lists = _.cloneDeep(oData[0].호수추천);
+      _.each(lists, (obj) => {
+        let valueSum = 0;
+        _.map(obj, (v, k) => {
+          const found = keyArray.find(element => element === k);
+          if (found) {
+            valueSum += (v * mUsed[k])
+          }
+        })
+        obj.UID = 'U' + info.기지 + info.대대 + obj.호기ID
+        obj.TOTAL = Number((valueSum / sUsed).toFixed(2));
+      });
+      console.log(_.sortBy(lists, 'TOTAL').reverse())
+
+
+      //
     } else if (topNum === 1) {
       const filterData = _.filter(DS, { LOCATION: '2B', SERIES: 'KF21' });
       setData(filterData)
@@ -56,7 +96,7 @@ const App = (props) => {
       const filterData = _.filter(DS, { LOCATION: '4B', SERIES: 'T50' });
       setData(filterData)
     } else {
-      setData(DS);
+      //setData(DS);
     }
   }, [topNum]);
 
